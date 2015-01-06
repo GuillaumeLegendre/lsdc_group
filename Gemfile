c4fc88d10a2f0bdc02ac31c13a2622ecd4068c94
root <root@vps128250.ovh.net>
2015-01-06 23:34:33 +0100

    .

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-# gem 'therubyracer',  platforms: :ruby
+gem 'therubyracer',  platforms: :ruby
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -51,7 +51,7 @@ gem 'bootstrap-sass-extras'
 gem 'simple_form'
 gem 'dropzonejs-rails'
 gem 'carrierwave'
-gem "rmagick"
+#gem "rmagick"
 gem 'font-awesome-rails'
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,14 @@ ActiveRecord::Schema.define(version: 20141225153859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "galleries", force: :cascade do |t|
+  create_table "galleries", force: true do |t|
   end
 
-  create_table "paintings", force: :cascade do |t|
+  create_table "paintings", force: true do |t|
     t.string "image"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
